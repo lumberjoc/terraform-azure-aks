71bb5e36557a784e2e0c11bc0cd1604a85aff282
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-03-25 12:08:48 -0700

    update: resource group

---
+++ 2-resource-group.tf
@@ -0,0 +1,7 @@
+# Resource group is a container that holds related resources for 
+# Azure - usually when they have similar lifecycles 
+
+resource "azurerm_resource_group" "this" {
+  name = local.resource_group_name
+  location = local.region
+}
