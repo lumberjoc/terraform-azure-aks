15e6dea815849855fb96557a16aca42f94a8f1df
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-03-25 12:19:31 -0700

    fmt

--- 4-subnets.tf
+++ 4-subnets.tf
@@ -3,15 +3,15 @@
 # have internet access by default. Just need public IP address
 
 resource "azurerm_subnet" "subnet1" {
-  name = "subnet1"
-  address_prefixes = {"10.0.0.0/19"}
-  resource_group_name = azurerm_resource_group.this.name
+  name                 = "subnet1"
+  address_prefixes     = ["10.0.0.0/19"]
+  resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name = "subnet2"
-  address_prefixes = {"10.0.32.0/19"}
-  resource_group_name = azurerm_resource_group.this.name
+  name                 = "subnet2"
+  address_prefixes     = ["10.0.32.0/19"]
+  resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
 }
