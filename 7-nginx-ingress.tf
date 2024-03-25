2956983c1ad972741a09f59e70066434cb85764e
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-03-25 13:20:25 -0700

    type

--- 7-nginx-ingress.tf
+++ 7-nginx-ingress.tf
@@ -1,5 +1,5 @@
 data "azurerm_kubernetes_cluster" "this" {
-  name                = "${local.env}-${local.eks_name}"
+  name                = "${local.env}-${local.aks_name}"
   resource_group_name = local.resource_group_name
 
   # Comment this out if you get: Error: Kubernetes cluster unreachable 
